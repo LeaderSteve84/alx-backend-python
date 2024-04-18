48311b71a225e103caf9d55a742504ba43fc7c68
stephen_adah <mailstephenreal@gmail.com>
2024-04-18 17:34:19 +0100

    readme.md, task 1, task 2, task 3

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module of type-annotated function that
+returns the sum of a float arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floating-point numbers and return their sum."""
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module of type-annotated function that returns
+the floor of the float.
+"""
+
+
+import math
+def floor(n: float) -> int:
+    """Return the floor of a given float."""
+    return math.floor(n)
