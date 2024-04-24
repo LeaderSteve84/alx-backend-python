d82876297d83589c151040376bcaadae143e66f7
stephen_adah <mailstephenreal@gmail.com>
2024-04-24 11:03:25 +0100

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,11 +4,12 @@ that takes no arguments.
 """
 
 
+from typing import AsyncGenerator
 import asyncio
 import random
 
 
-async def async_generator() -> None:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine that yields a random number between 0 and 10
     every second for 10 seconds.
     """
