73cd68f20c1470bba46e3932db2f507a5b772bae
stephen_adah <mailstephenreal@gmail.com>
2024-05-14 14:46:49 +0100

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -44,3 +44,22 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get') as mockRequest:
             mockRequest.return_value.json.return_value = test_payload
             self.assertEqual(get_json(url=test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """class with test memoized method"""
+    def test_memoize(self):
+        """test memoize decorator"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mockMemo:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mockMemo.assert_called_once()
+
